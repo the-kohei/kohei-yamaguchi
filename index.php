3476e38715aebda6365422f180d20e504098d267
the-kohei <ky19930221ky@gmail.com>
2020-06-06 16:25:43 +0900

    修正

---
+++ index.php
@@ -0,0 +1,55 @@
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Kohei Yamaguchi</title>
+  </head>
+  <body>
+    <header>
+      <div class="heading">
+        <h1 class="title">山口 晃平</h1>
+        
+      </div>
+
+      <div class="navbar">
+        <p class="title-name">Yamaguchi Kohei</p>
+        <ul>
+          <li><a href="index.php">ホーム</a></li>
+          <li><a href="myself.html">経歴</a></li>
+          <li><a href="portfolio.html">ポートフォリオ </a></li>
+        </ul>
+
+      </div>
+    </header>
+
+    <div class="contents">
+      <div class="left-content">
+        <img src="yamaguchi.jpg" alt="name" class="image">
+      </div>
+
+      <div class="right-content">
+        <p class="index">welcome !</p>
+        <div class="manu">
+          <a class="btn1" href="myself.html">経歴</a>
+          <a class="btn2" href="portfolio.html">ポートフォリオ</a>
+        </div>
+        <div class="content">
+          <p class="text">私、山口晃平のことを知っていただくために簡易的なホームページを作成しました。<br>履歴書、職務経歴書、ポートフォリオやGitHubも公開していますので是非見ていっていただければと思います。</p>
+        </div>
+      </div>
+    </div>
+
+    <footer>
+      <div class="footer-contents">
+        <div class="tel">
+          <img src="tel.jpg" alt="tel" class="tel-icon">
+          <p class="phone">電話<br>TEL : 080-3906-8500</p>
+        </div>
+        <div class="mail">
+          <img src="gmail.jpg" alt="gmail" class="gmail-icon">
+          <p class="gmail">メール<br>mail : ky19930221ky@gmail.com</p>
+        </div>
+      </div>
+    </footer>
+  </body>
+</html>
